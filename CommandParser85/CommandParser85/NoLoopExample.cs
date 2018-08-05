80219243aed9004b890a27bdf4e78e9aa49e4eca
Akhill Shetty <akkhilayshetty@gmail.com>
2018-08-05 12:06:26 -0400

    Add files via upload

---
+++ CommandParser85/CommandParser85/NoLoopExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CommandParser85
+{
+    class NoLoopExample
+    {
+        public void NoLoop(List<string> list, string example_file)
+        {
+            var path = $"{Path.GetDirectoryName(example_file)}\\{Path.GetFileNameWithoutExtension(example_file)}Complete{Path.GetExtension(example_file)}";
+
+            var commentPattern = @"\s*^(#.+)";
+
+            List<string> new_list = new List<string>();
+
+            foreach (var list_item in list)
+            {
+                Match commentCheck = Regex.Match(list_item, commentPattern, RegexOptions.Singleline);
+
+                if (!commentCheck.Success)
+                    new_list.Add(list_item);
+            }
+
+            foreach (var list_item in new_list)
+            {
+                //Console.WriteLine(list_item);
+                File.AppendAllText(path, list_item.ToString() + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ CommandParser85/CommandParser85/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CommandParser85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleLoopFilePath = @"K:\NoLoopExample.txt";
+            string readAllText = File.ReadAllText(singleLoopFilePath);
+
+            List<string> list = new List<string>(Regex.Split(readAllText, Environment.NewLine));
+
+            NoLoopExample obj = new NoLoopExample();
+            obj.NoLoop(list, singleLoopFilePath);
+        }
+    }
+}
