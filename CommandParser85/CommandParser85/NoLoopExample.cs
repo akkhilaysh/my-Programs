9d0621a5f5091643a859f45ce84f1e4941ed7b13
Akhill Shetty <akkhilayshetty@gmail.com>
2018-08-05 12:42:04 -0400

    Update NoLoopExample.cs

--- CommandParser85/CommandParser85/NoLoopExample.cs
+++ CommandParser85/CommandParser85/NoLoopExample.cs
@@ -13,13 +13,14 @@ namespace CommandParser85
         public void NoLoop(List<string> list, string example_file)
         {
             var path = $"{Path.GetDirectoryName(example_file)}\\{Path.GetFileNameWithoutExtension(example_file)}Complete{Path.GetExtension(example_file)}";
+            
             var commentPattern = @"\s*^(#.+)";
+            Match commentCheck = Regex.Match(list_item, commentPattern, RegexOptions.Singleline);
+            
             List<string> new_list = new List<string>();
 
             foreach (var list_item in list)
-            {
-                Match commentCheck = Regex.Match(list_item, commentPattern, RegexOptions.Singleline);
-
+            {               
                 if (!commentCheck.Success)
                     new_list.Add(list_item);
             }
